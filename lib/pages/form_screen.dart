19d38dd0229c1bcfa37527e9f75dab956d2b1652
rakibhossan545196 <hossanr082@gmail.com>
2024-09-07 02:33:29 +0600

    appbar add

--- lib/pages/form_screen.dart
+++ lib/pages/form_screen.dart
@@ -11,6 +11,30 @@ class FormScreen extends StatefulWidget {
 }
 
 class _FormScreenState extends State<FormScreen> {
+  int _selectedIndex = 0;
+  final List<Widget> _views = <Widget>[
+    const Center(
+
+      child: Image(
+        image: NetworkImage('https://www.shutterstock.com/image-photo/back-view-unrecognizable-solitary-hiker-260nw-2460279067.jpg'),
+      ),
+
+    ),
+    const Center(
+
+      child: Image(
+        image: NetworkImage('https://t4.ftcdn.net/jpg/07/36/82/89/360_F_736828938_FqSfw9ZzpzOi1uaCeYD80UMRcdJ0bAaF.jpg'),
+      ),
+
+    ),
+    const Center(
+
+      child: Image(
+        image: NetworkImage('https://t4.ftcdn.net/jpg/07/36/82/89/360_F_736828938_FqSfw9ZzpzOi1uaCeYD80UMRcdJ0bAaF.jpg'),
+      ),
+
+    ),
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,8 +44,46 @@ class _FormScreenState extends State<FormScreen> {
         backgroundColor: Colors.lightGreen,
       ),
       drawer: drawer(),
+      body: SafeArea(
+        child: _views.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        elevation: 2,
+        iconSize: 15,
+        selectedItemColor: Colors.blueAccent,
+        selectedLabelStyle: TextStyle(color: Colors.black),
+        selectedIconTheme: IconThemeData(
+          color: Colors.blue,
+        ),
+        backgroundColor: Colors.blueGrey,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.home,
+              color: Colors.amber,
+            ),
+            label: "Home",
+          ),
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.chat,
+                color: Colors.amber,
+              ),
+              label: "Chat"),
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.notification_important,
+                color: Colors.amber,
+              ),
+              label: "Notification"),
+        ],
+        currentIndex: _selectedIndex,
+        onTap: (index) {
+          setState(() {
+            _selectedIndex = index;
+          });
+        },
+      ),
     );
   }
 }
-
-
